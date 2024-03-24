ce057fde94de50c4c9e083225d7e5995166a3630
j5155 <jamesrosson012@gmail.com>
2024-03-24 16:18:39 +0000

    add dashboard server

---
+++ DashServer/src/main/java/org/example/TestServer.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class TestServer {
+    public static void main(String[] args) throws InterruptedException {
+        TestDashboardInstance.getInstance().start();
+    }
+}
