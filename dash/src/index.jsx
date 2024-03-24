356625182e0d7ca435bd7836aa48f41ddaad6b50
j5155 <jamesrosson012@gmail.com>
2024-03-24 15:35:45 +0000

    add dashboard

---
+++ dash/src/index.jsx
@@ -0,0 +1,20 @@
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import Dashboard from './components/Dashboard/Dashboard';
+import configureStore from './store/configureStore';
+import { ThemeProvider } from '@/hooks/useTheme';
+
+import { DiscordSDK } from "@discord/embedded-app-sdk";
+
+import './index.css';
+
+const store = configureStore();
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <ThemeProvider>
+    <Provider store={store}>
+      <Dashboard />
+    </Provider>
+  </ThemeProvider>,
+);
---
+++ dash/vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+
+export default defineConfig({
+  base: '/',
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: path => path. replace(/^\/ api/, '')
+      },
+    },
+    hmr: {
+      clientPort: 443,
+    },
+    publicDir: "dash"
+  },
+});
